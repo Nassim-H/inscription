bd685606bd2cc0705f9961c189b42af9ed40b7fe
nassim-h <nassimhassain@gmail.com>
2025-06-29 14:10:09 +0100

    Required + PADDING INPUT

--- app/components/ui/Input.tsx
+++ app/components/ui/Input.tsx
@@ -19,7 +19,7 @@ export default function Input({
   return (
 <div className="w-full py-4">
       {label && (
-        <label htmlFor={id} className="block text-sm font-medium text-gray-800">
+        <label htmlFor={id} className="block text-sm font-medium text-gray-800 pb-2 pl-1">
           {label}{required && <span className="text-red-500 ml-0.5">*</span>}
         </label>
       )}
@@ -27,7 +27,6 @@ export default function Input({
         id={id}
         type={type}
         value={value}
-        required
         onChange={onChange}
         required={required}
         className={`w-full rounded-xl border border-gray-300 px-4 py-2 text-sm sm:text-base placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition ${className}`}
